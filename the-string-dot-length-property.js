8e1757ee4f2bfdae0f7cec7b3c5de7ab46fb046c
lsmith1580 <lindseysmith1580@gmail.com>
2025-03-23 19:02:25 -0500

    Add files via upload

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -26,5 +26,26 @@ let lengthThree; // Length of stringThree
 
 // Your code here
 
+console.log(stringOne.length);
+
+console.log(stringTwo.length);
+
+console.log(stringThree.length);
+
+lengthOne = stringOne.length; 
+
+console.log(lengthOne);
+
+lengthTwo = stringTwo.length;
+
+console.log(lengthTwo);
+
+lengthThree = stringThree.length;
+
+console.log(lengthThree);
+
+
+
+
 
 
